2e57ae847a60f51f9859203f9d427c9428e9f7d5
Serik Ibragimov <serik.kerimbayevich@onlyoffice.com>
2026-04-20 19:07:53 +0500

    fix: rename shardKey to shardkey

--- Module.php
+++ Module.php
@@ -405,7 +405,7 @@ class Module extends \humhub\components\Module
 
     public function convertService($documentUrl, $fromExt, $toExt, $key, $async = true): array
     {
-        $url = $this->getInternalServerUrl() . '/converter' . '?shardKey=' . $key;
+        $url = $this->getInternalServerUrl() . '/converter' . '?shardkey=' . $key;
 
         $user = Yii::$app->user->getIdentity();
         $lang = ($user) && !empty($user->language) ? $user->language : Yii::$app->language;
